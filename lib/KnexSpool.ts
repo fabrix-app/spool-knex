66f34d2f1c74236bd8b01263d7b1bf1344f16556
scott-wyatt <scottwyatt86@gmail.com>
2019-06-21 10:58:26 -0500

    feat(): latest version of knex

--- lib/KnexSpool.ts
+++ lib/KnexSpool.ts
@@ -1,4 +1,4 @@
-import * as knex from 'knex'
+import knex from 'knex'
 import { DatastoreSpool } from '@fabrix/fabrix/dist/common/spools/datastore'
 import { pickBy, mapValues, map } from 'lodash'
 import { Utils } from './utils'
